ddc16fef9aa62e20c5ecf2a59fe61fe9519733b6
Moynul Islam <moynul200215@gmail.com>
2026-05-11 13:21:31 +0600

    server the forntend from a http server

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+// serve frontend
+app.use(express.static(path.join(__dirname, "public")));
+
+// serve images folder separately
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
