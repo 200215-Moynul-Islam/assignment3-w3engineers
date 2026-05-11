58e285ee07fdd3195a6b533d6f48fb07cf9ea917
Moynul Islam <moynul200215@gmail.com>
2026-05-11 15:50:33 +0600

    implement get property endpoint

--- server.js
+++ server.js
@@ -27,6 +27,36 @@ app.get("/images", (req, res) => {
   res.json(imageUrls);
 });
 
+app.get("/get-property", (req, res) => {
+  const {
+    "most-popular": mostPopular,
+    "highest-price": highestPrice,
+    "lowest-price": lowestPrice,
+    limit,
+  } = req.query;
+  let filePath;
+  if (mostPopular === "true") {
+    filePath = path.join(__dirname, "data", "most_popular.json");
+  } else if (highestPrice === "true") {
+    filePath = path.join(__dirname, "data", "highest_price.json");
+  } else if (lowestPrice === "true") {
+    filePath = path.join(__dirname, "data", "lowest_price.json");
+  } else {
+    return res
+      .status(400)
+      .json({ error: "Please provide a valid query parameter" });
+  }
+  const rawData = fs.readFileSync(filePath, "utf-8");
+  let properties = JSON.parse(rawData);
+
+  // Limit the Result.Items array
+  if (limit && properties.Result && properties.Result.Items) {
+    properties.Result.Items = properties.Result.Items.slice(0, Number(limit));
+  }
+
+  res.json(properties);
+});
+
 // Start server
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
