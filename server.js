e59b1c2360a356c0717d0ae3b9863e34abf0860f
Moynul Islam <moynul200215@gmail.com>
2026-05-11 15:16:45 +0600

    implement get images endpoints

--- server.js
+++ server.js
@@ -1,19 +1,33 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 
 const app = express();
 const PORT = 3000;
 
-// serve frontend
+// Serve frontend files from public folder
 app.use(express.static(path.join(__dirname, "public")));
 
-// serve images folder separately
+// Serve images folder
 app.use("/images", express.static(path.join(__dirname, "images")));
 
+// Home route
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+// Return image URLs
+app.get("/images", (req, res) => {
+  const imagesDir = path.join(__dirname, "images/sanctuary-cap-cana");
+  const fileNames = fs.readdirSync(imagesDir);
+  const imageUrls = fileNames.map(
+    (fileName) => `/images/santuary-cap-cana/${fileName}`
+  );
+
+  res.json(imageUrls);
+});
+
+// Start server
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
